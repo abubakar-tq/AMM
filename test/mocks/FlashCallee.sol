bb6744021f1d90548ae6b0325e4d7bc419b6fc59
Abubakar <rabubakar351@gmail.com>
2026-02-27 01:19:48 +0500

    Implemented SafeTransfer in mocks/FlashCalle.sol

--- test/mocks/FlashCallee.sol
+++ test/mocks/FlashCallee.sol
@@ -3,8 +3,11 @@ pragma solidity 0.8.33;
 
 import {IV2Callee} from "src/interfaces/IV2Callee.sol";
 import {MockERC20} from "test/mocks/MockERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract FlashCallee is IV2Callee {
+    using SafeERC20 for MockERC20;
+
     MockERC20 token0;
     MockERC20 token1;
     address pair;
@@ -19,7 +22,7 @@ contract FlashCallee is IV2Callee {
         require(msg.sender == pair, "Only pair");
         (uint256 amount0In, uint256 amount1In) = abi.decode(data, (uint256, uint256));
 
-        if (amount0In > 0) token0.transfer(pair, amount0In);
-        if (amount1In > 0) token1.transfer(pair, amount1In);
+        if (amount0In > 0) token0.safeTransfer(pair, amount0In);
+        if (amount1In > 0) token1.safeTransfer(pair, amount1In);
     }
 }
